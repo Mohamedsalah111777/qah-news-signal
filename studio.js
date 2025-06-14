fcd983705221024877865c7e161f80b8947ae8ee
Mohamedsalah111777 <mohamed.salah111777@gmail.com>
2025-06-15 00:23:37 +0300

    Add files via upload

---
+++ studio.js
@@ -0,0 +1,6 @@
+let remoteVideo = document.getElementById('remoteVideo');
+
+// لاحقًا: استلام الفيديو من الضيف عبر WebRTC
+// في النسخة الأولية نجهز المكان فقط
+
+console.log("Studio page loaded. Ready to receive guest stream.");
